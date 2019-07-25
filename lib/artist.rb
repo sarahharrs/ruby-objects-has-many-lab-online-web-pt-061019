5f756aa550f6c816ce0cde71fe197685ce56a7dd
Sarah Harrs <sarahharrs@gmail.com>
2019-07-25 20:36:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,14 @@
 class Artist
-  attr_accessor :name :song
+  attr_accessor :name
+
+  def inirialize (name)
+    @name = name
+    @songs = []
+  end
+
+  def add_songs
+    @songs <<songs
+  end
+
+
   
